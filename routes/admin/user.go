6175b8aa3cd6a15aa8e5c19bbc70c4a7e11ab08d
iknow0614 <xiongwei_13@hotmail.com>
2016-05-17 16:00:21 +0800

    完成新增用户和删除用户接口

--- routes/admin/user.go
+++ routes/admin/user.go
@@ -23,10 +23,10 @@ func UserSubrouter(r *mux.Router, db *sql.DB) {
 	// 验证密码并获取用户信息
 	subrouter.HandleFunc("/confirm-passwd", func(w http.ResponseWriter, r *http.Request) {
 
-    res := map[string]string{
-      "stat": "false",
-      "info": "错误的输入格式",
-    }
+		res := map[string]string{
+			"stat": "false",
+			"info": "错误的输入格式",
+		}
 		template := map[string]string{
 			"user_id":     "",
 			"user_passwd": "",
@@ -69,6 +69,107 @@ func UserSubrouter(r *mux.Router, db *sql.DB) {
 		}
 
 		genres := httpjsondone.GenRes(data, res, template)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(genres)
+	})
+
+	// 新增用户
+	subrouter.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+
+		res := map[string]string{
+			"stat": "false",
+			"info": "错误的输入格式",
+		}
+		template := map[string]string{
+			"user_id":     "",
+			"user_name":   "",
+			"user_status": "",
+			"user_passwd": "",
+			"tellerno":    "",
+		}
+
+		body := httpjsondone.GetBody(r)
+		if (body["user_id"] == "") || (body["user_name"] == "") || (body["user_passwd"] == "") || (body["user_status"] == "") {
+			genres := httpjsondone.GenRes(nil, res, template)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(genres)
+			return
+		}
+
+		userid := body["user_id"]
+		username := body["user_name"]
+		userstatus := body["user_status"]
+		userpasswd := md5passwd.Get(body["user_passwd"])
+		tellerno := body["tellerno"]
+
+		log.Println("add user:", userid)
+
+		stmt, _ := db.Prepare("insert into userlist (user_id, user_name, user_status, user_password, tellerno) values (?, ?, ?, ?, ?)")
+		_, err := stmt.Exec(userid, username, userstatus, userpasswd, tellerno)
+		stmt.Close()
+
+		if err != nil {
+			res["stat"] = "false"
+			res["info"] = err.Error()
+			genres := httpjsondone.GenRes(nil, res, template)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(genres)
+			return
+		}
+
+		res["stat"] = "true"
+		res["info"] = "用户添加成功"
+		genres := httpjsondone.GenRes(nil, res, template)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(genres)
+	})
+
+	// 删除用户
+	subrouter.HandleFunc("/del", func(w http.ResponseWriter, r *http.Request) {
+
+		res := map[string]string{
+			"stat": "false",
+			"info": "错误的输入格式",
+		}
+		template := map[string]string{
+			"user_id": "",
+		}
+
+		body := httpjsondone.GetBody(r)
+		if body["user_id"] == "" {
+			genres := httpjsondone.GenRes(nil, res, template)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(genres)
+			return
+		}
+
+		userid := body["user_id"]
+
+		tx, _ := db.Begin()
+		_, errUserList := tx.Exec("delete from userlist where user_id = '" + userid + "'")
+		_, errRelUserDep := tx.Exec("delete from rel_user_dep where user_id = '" + userid + "'")
+		_, errRelUserSysDept := tx.Exec("delete from rel_user_sys_dept where user_id = '" + userid + "'")
+		_, errSysManager := tx.Exec("delete from sys_manager where user_id = '" + userid + "'")
+		_, errRelUserRole := tx.Exec("delete from rel_user_role where user_id = '" + userid + "'")
+
+		if (errUserList != nil) ||
+			(errRelUserDep != nil) ||
+			(errRelUserSysDept != nil) ||
+			(errSysManager != nil) ||
+			(errRelUserRole != nil) {
+
+			tx.Rollback()
+			res["info"] = "执行失败"
+			genres := httpjsondone.GenRes(nil, res, template)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(genres)
+			return
+		}
+		tx.Commit()
+
+		res["stat"] = "true"
+		res["info"] = "删除成功"
+		genres := httpjsondone.GenRes(nil, res, template)
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(genres)
 	})
